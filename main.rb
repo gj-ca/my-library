52b2461ab379e26c93fbf3277bbc9386cfdc5022
gj-ca <glen.johnson@coderacademy.edu.au>
2020-09-18 12:12:32 +1000

    added login feature for users

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 class User
+    attr_reader :username, :password
     def initialize(username, password)
         @username = username
         @password = password
-
     end
 end
 
@@ -29,9 +29,28 @@ list_of_users = [
     Administrator.new("Mrs Leder", "I-am-the-library")
 ]
 
-# loop do
-#     puts "Enter your username"
-#     username = gets.chomp
-#     puts "Enter your password"
-#     password = gets.chomp
-# end
+loop do
+    puts "Enter your username"
+    input_username = gets.chomp
+    puts "Enter your password"
+    input_password = gets.chomp
+    # Saving a user variable by selecting a user that has a username of what we typed in
+    user = list_of_users.select { |user| user.username == input_username }[0]
+
+    # dot notation for classes
+    if user.username == input_username && user.password == input_password
+        puts "You are logged in test"
+    else
+        puts "incorrect"
+    end
+
+
+    #square bracketes for hashes
+    # user[:username]
+
+
+
+
+    # if the username and the password matches our found user
+    # if user[:username]
+end
